aa6de75610fd137a6cbcb463ce2f4ca5fa341b7b
LewisCM14 <Lewiscmartin@live.co.uk>
2022-08-21 16:16:46 +0100

    handle Checkout.js form validation

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,45 +1,118 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
-import classes from './Checkout.module.css';
+import classes from "./Checkout.module.css";
+
+// Helper functions
+const isEmpty = (value) => value.trim() === "";
+const isSevenChars = (value) => value.trim().length === 7;
 
 const Checkout = (props) => {
-    /**
-     * Confirm Checkout function, prevents default on from submit
-     * 
-     */
+  /**
+   * Confirm Checkout function, prevents default on from submit
+   * references the current value for the input fields
+   * Then validates these values with the helper functions above,
+   * returning truthy variables if valid. These variables then validate the form submission.
+   * form errors are managed with the formInputsValidity state.
+   */
 
-    const confirmHandler = (event) => {
-        event.preventDefault();
-    };
+  // form validity state, defaults true as no errors initially.
+  const [formInputsValidity, setFormInputsValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true,
+  });
 
-    // Checkout form, receives the onCancel prop from Cart.js,
-    //which points at the hideCartHandler in App.js
-    return (
-        <form className={classes.form} onSubmit={confirmHandler}>
-        <div className={classes.control}>
-          <label htmlFor='name'>Your Name</label>
-          <input type='text' id='name' />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='street'>Street</label>
-          <input type='text' id='street' />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='postal'>Postal Code</label>
-          <input type='text' id='postal' />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='city'>City</label>
-          <input type='text' id='city' />
-        </div>
-        <div className={classes.actions}>
-          <button type='button' onClick={props.onCancel}>
-            Cancel
-          </button>
-          <button className={classes.submit}>Confirm</button>
-        </div>
-      </form>
-    );
+  // form ref values, reads input values
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+    const enteredPostalCodeIsValid = isSevenChars(enteredPostalCode);
+
+    // input validity object, points at the formInputValidityState
+    // Helps render error messages
+    setFormInputsValidity({
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      city: enteredCityIsValid,
+      postalCode: enteredPostalCodeIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredCityIsValid &&
+      enteredPostalCodeIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+  };
+
+  // form control class rendering
+  const nameControlClasses = `${classes.control} ${
+    formInputsValidity.name ? "" : classes.invalid
+  }`;
+
+  const streetControlClasses = `${classes.control} ${
+    formInputsValidity.street ? "" : classes.invalid
+  }`;
+
+  const postalCodeControlClasses = `${classes.control} ${
+    formInputsValidity.postalCode ? "" : classes.invalid
+  }`;
+
+  const cityControlClasses = `${classes.control} ${
+    formInputsValidity.city ? "" : classes.invalid
+  }`;
+
+  // Checkout form, receives the onCancel prop from Cart.js,
+  //which points at the hideCartHandler in App.js
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={nameControlClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" ref={nameInputRef} />
+        {!formInputsValidity.name && <p>Please enter a valid name!</p>}
+      </div>
+      <div className={streetControlClasses}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" ref={streetInputRef} />
+        {!formInputsValidity.street && <p>Please enter a valid street!</p>}
+      </div>
+      <div className={postalCodeControlClasses}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" ref={postalCodeInputRef} />
+        {!formInputsValidity.postalCode && (
+          <p>Please enter a valid postal code (7 characters long)!</p>
+        )}
+      </div>
+      <div className={cityControlClasses}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityInputRef} />
+        {!formInputsValidity.city && <p>Please enter a valid city!</p>}
+      </div>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
 };
 
 export default Checkout;
